9efac27d564b4246482badb1124bb3215a2be64a
Parker Miles Layden-Tapp <layd4810@mylaurier.ca>
2020-12-08 21:01:54 -0500

    changes

--- partials/Page.js
+++ partials/Page.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 module.exports = class{
     constructor(){
         this.sName = "Parker Layden-Tapp";
+        this.sUrlToEmailer = "https://quiet-fortress-89301.herokuapp.com/send/";
     }
     requireMarked(sFname){
         const sDir = process.cwd()
